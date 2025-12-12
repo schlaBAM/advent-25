1f29cb53d37d6624588ad940ad31d6aebde785bb
Brett Morrison <brett.morrison@live.com>
2025-12-11 22:06:03 -0800

    day 3 part 2 working solution

--- d3/part2.rb
+++ d3/part2.rb
@@ -5,7 +5,46 @@ def main
 end
 
 def joltage(num)
+  digits = num.digits.reverse
+  hash = Hash.new { |h, k| h[k] = [] }
 
+  max_index = 0
+  max_digit = 1
+  0.upto(digits.length - 13) do |i|
+    if digits[i] > max_digit
+      max_digit = digits[i]
+      max_index = i
+    end
+  end
+
+  digits = digits[max_index..]
+
+  digits.each_with_index do |num, idx|
+    hash[num] << idx
+  end
+
+  idx = 1
+
+  until digits.length == 12
+    if digits.length - hash[idx].length > 12
+      digits.delete(idx)
+    else
+      delete_num = digits.length - 12
+      deleted = 0
+
+      while delete_num > 0 && !hash[idx].empty?
+        digits.delete_at(hash[idx].first - deleted)
+        hash[idx].shift
+        delete_num -= 1
+        deleted += 1
+      end
+    end
+    idx += 1
+  end
+
+  num = digits.join.to_i
+  puts num
+  num
 end
 
 puts main
