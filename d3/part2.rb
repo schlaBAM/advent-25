58a2e671d7d8e83f628dfeadcc7b5f8f9f053c32
Brett Morrison <brett.morrison@live.com>
2025-12-12 01:27:21 -0800

    changed greedy style for day 3 part 2

--- d3/part2.rb
+++ d3/part2.rb
@@ -3,58 +3,33 @@
 BANK_SIZE = 12
 
 def main
-  File.readlines('input.txt', chomp: true).sum { |bank| joltage(bank.to_i) }
+  File.readlines('input.txt', chomp: true).sum { |bank| joltage(bank) }
 end
 
-def joltage(num)
-  digits = num.digits.reverse
-  digits_index = Hash.new { |h, k| h[k] = [] }
+def joltage(bank)
+  digits = highest_splice(bank.chars.map(&:to_i))
 
-  digits = highest_splice(digits)
+  digits_left = digits.length - BANK_SIZE
+  result = []
 
-  digits.each_with_index do |num, idx|
-    digits_index[num] << idx
-  end
-
-  current_index = 1
-  index_dump = []
-  bank_length = digits.length
-
-  # puts "BANK SIZE: #{bank_length}"
-
-  while bank_length > BANK_SIZE
-    if bank_length - digits_index[current_index].length > BANK_SIZE
-      # puts "IF #{current_index} #{digits_index[current_index]} #{digits_index[current_index].length}"
-      index_dump << digits_index[current_index]
-      bank_length -= digits_index[current_index].length
-    else
-      digits_to_delete = bank_length - BANK_SIZE
-      # IDEA: put indexes into an array, and delete from the back
-      puts " ELSE #{current_index} #{digits_index[current_index][0...digits_to_delete]} #{digits_index[current_index][0...digits_to_delete].length}"
-      index_dump << digits_index[current_index][0...digits_to_delete]
-      bank_length -= digits_index[current_index][0...digits_to_delete].length
+  digits.each do |d|
+    while !result.empty? && digits_left.positive? && result.last < d
+      result.pop
+      digits_left -= 1
     end
-    # puts "nums: #{index_dump}"
-    # puts bank_length
-    current_index += 1
+    result << d
   end
 
-  index_dump = index_dump.flatten.sort
-
-  index_dump.reverse.each do |idx|
-    digits.delete_at(idx)
-  end
-
-  x = digits.join.to_i
-  puts x
-
-  x
+  result[0...BANK_SIZE].join.to_i
 end
 
+# there's likely a ton of unusable digits between idx[0] and idx[-BANK_SIZE]. let's remove them.
 def highest_splice(digits)
   max_index = 0
   max_digit = 1
-  0.upto(digits.length - BANK_SIZE - 1) do |i|
+  index_cutoff = digits.length - BANK_SIZE - 1
+
+  0.upto(index_cutoff) do |i|
     if digits[i] > max_digit
       max_digit = digits[i]
       max_index = i
