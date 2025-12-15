5200005b2faa1c19c0517878f5ea1c8df385bfea
Brett Morrison <brett.morrison@live.com>
2025-12-15 00:49:11 -0800

    day 5 part 1 with range optimization

--- d5/part1.rb
+++ d5/part1.rb
@@ -4,18 +4,25 @@ def main
   ranges = input[0].map { |r| r.split('-').map(&:to_i) }.sort
   ids = input[1].map(&:to_i)
 
-  # updated_ranges = condense_ranges(ranges.sort)
-  count_freshness(ids, ranges)
+  updated_ranges = merge(ranges)
+  count_freshness(ids, updated_ranges)
 end
 
-def condense_ranges(input)
-
-  # with the inputs sorted, we know that the min range is larger than the previous.
+def merge(ranges)
+  merged = []
+  ranges.each do |first, last|
+    if merged.empty? || first > merged[-1][1] + 1
+      merged << [first, last]
+    else
+      merged[-1][1] = [merged[-1][1], last].max
+    end
+  end
+  merged
 end
 
 def count_freshness(list, ranges)
   list.count do |id|
-    ranges.any? { |first, last| id >= first && id <= last}
+    ranges.any? { |first, last| id >= first && id <= last} # binary search this instead if list grows
   end
 end
 
